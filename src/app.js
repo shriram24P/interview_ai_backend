0507defec83020bfd59aa19eba9a9f6f2c48679e
Shriram Patil <patil.shriram98@gmail.com>
2026-06-09 22:32:13 +0530

    cors issue

--- src/app.js
+++ src/app.js
@@ -3,29 +3,14 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://interview-ai-bice-tau.vercel.app",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  }),
-);
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(cookieParser());
 
 // require all the routes here
 const authRouter = require("./routes/auth.routes");
 const interviewRouter = require("./routes/interview.routes");
+const testRouter = require("./routes/test.routes");
 
 // using all the routes here
 app.use("/api/auth", authRouter);
