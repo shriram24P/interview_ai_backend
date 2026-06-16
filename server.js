5c799d9594cee992ae71d5f7c179526d1aef8f77
Shriram Patil <patil.shriram98@gmail.com>
2026-06-16 22:35:22 +0530

    port added

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const connectDB = require('./src/config/database');
 connectDB();
 
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Server is running on port ' + (process.env.PORT || 3000));
+app.listen(process.env.PORT || 8000, ()=>{
+    console.log('Server is running on port ' + (process.env.PORT || 8000));
 })
