55f89366109e75d264ec3b65b4e9f4569c024550
Shriram Patil <patil.shriram98@gmail.com>
2026-06-17 19:01:35 +0530

    changes

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ connectDB();
 
 
 app.listen(process.env.PORT || 8000, ()=>{
-    console.log('Server is running on port ' + (process.env.PORT || 8000));
+    console.log('Server is running on port : ' + (process.env.PORT || 8000));
 })
